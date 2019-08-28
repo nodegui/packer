05dbdf8f1d881ba7c1a3da0e901aca1030ce4cb0
Atul R <atulanand94@gmail.com>
2019-08-28 23:30:59 +0200

    Adds basic macos packaging support

---
+++ README.md
@@ -0,0 +1,13 @@
+# Packer
+
+Go to your nodegui app repo
+
+and then
+
+```
+npm install --save-dev @nodegui/packer
+
+npx nodegui-packer --init <AppName>
+
+npx nodegui-packer --pack ./dist
+```
---
+++ src/cli.ts
@@ -0,0 +1,17 @@
+import program from "commander";
+import { init, pack } from "./index";
+
+program
+  .option("-i, --init <name>", "Creates initial deploy files")
+  .option("-p, --pack <distPath>", "Packs the app into deployable");
+
+program.parse(process.argv);
+const options = program.opts();
+
+if (program.init) {
+  init(options.init);
+}
+
+if (program.pack) {
+  pack(options.pack);
+}
